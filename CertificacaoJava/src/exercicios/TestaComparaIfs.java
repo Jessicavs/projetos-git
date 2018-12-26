e7a68d76c353cbc4c26fbe29db07c0056d498fe1
Jessica Vieira <jessicavieirasoares@hotmail.com>
2018-12-25 23:14:50 -0200

    criacao de projeto

---
+++ CertificacaoJava/src/exercicios/TestaComparaIfs.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+public class TestaComparaIfs {
+
+	static public void main(String[] args) {
+		boolean autentico = true;
+
+		if (autentico) {
+			System.out.println("Ok!");
+		} else {
+			System.out.println("Não autorizado.");
+		}
+
+	}
+
+}
